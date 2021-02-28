2dadba597e02b7b4efb624258e825947fb106b4e
Alexandru Rosu <r.alex128@gmail.com>
2021-02-28 16:57:49 +0000

    Add private constructor in Constants

--- app/src/main/java/com/alexandruro/whistscoretracker/config/Constants.java
+++ app/src/main/java/com/alexandruro/whistscoretracker/config/Constants.java
@@ -2,6 +2,8 @@ package com.alexandruro.whistscoretracker.config;
 
 public class Constants {
 
+    private Constants() {}
+
     public static final int MIN_PLAYERS_COUNT = 3;
     public static final int MAX_PLAYERS_COUNT = 6;
 }
