b54a8acb2d73b576a7d55be52098e0f10bbdc736
Alexandru Rosu <r.alex128@gmail.com>
2023-02-01 23:22:25 +0200

    Revert dev flags

--- app/src/main/java/com/alexandruro/whistscoretracker/config/DevelopmentFlags.java
+++ app/src/main/java/com/alexandruro/whistscoretracker/config/DevelopmentFlags.java
@@ -7,7 +7,7 @@ public abstract class DevelopmentFlags {
     }
 
     // Add some player names to the new game form
-    public static final boolean PREPOPULATE_PLAYER_NAMES = true;
+    public static final boolean PREPOPULATE_PLAYER_NAMES = false;
 
     // Add some results to the game table when starting a new game
     public static final boolean PREPOPULATE_GAME_TABLE = PREPOPULATE_PLAYER_NAMES && true;
